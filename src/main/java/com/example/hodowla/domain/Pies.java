5716f70434461594216b582d0a61958cd0aafdd2
sherrywolf <daria.wieliczko@wp.pl>
2016-11-23 23:56:33 +0100

    dodanie pies.java i rasa.java

---
+++ src/main/java/com/example/hodowla/domain/Pies.java
@@ -0,0 +1,74 @@
+package com.example.hodowla.domain;
+
+public class Pies {
+
+    private int pies_id;
+    private String imie;
+    private int rok;
+    private String plec;
+    private String dieta;
+    private int rasa_id;
+
+    public Pies() {
+        super();
+    }
+
+    public Pies(int pies_id, String imie, int rok, String plec, String dieta, int rasa_id) {
+        super();
+
+        this.pies_id = pies_id;
+        this.imie = imie;
+        this.rok = rok;
+        this.plec = plec;
+        this.dieta = dieta;
+        this.rasa_id = rasa_id;
+    }
+
+    public int getpies_id() {
+        return pies_id;
+    }
+
+    public void setpies_id(int pies_id) {
+        this.pies_id = pies_id;
+    }
+
+    public String getimie() {
+        return imie;
+    }
+
+    public void setimie(String imie) {
+        this.imie = imie;
+    }
+
+    public int getrok() {
+        return rok;
+    }
+
+    public void setrok(int rok) {
+        this.rok = rok;
+    }
+
+    public String getplec() {
+        return plec;
+    }
+
+    public void setplec(String plec) {
+        this.plec = plec;
+    }
+
+    public String getdieta() {
+        return dieta;
+    }
+
+    public void setdieta(String dieta) {
+        this.dieta = dieta;
+    }
+
+    public int getrasa_id() {
+        return rasa_id;
+    }
+
+    public void setrasa_id(int rasa_id) {
+        this.rasa_id = rasa_id;
+    }
+}
---
+++ src/main/java/com/example/hodowla/domain/Rasa.java
@@ -0,0 +1,28 @@
+package com.example.hodowla.domain;
+
+public class Rasa {
+
+    private int rasa_id;
+    private String nazwa;
+    private String opis;
+
+    public Rasa() { super(); }
+
+    public Rasa(int rasa_id, String nazwa, String opis) {
+        super();
+
+        this.rasa_id = rasa_id;
+        this.nazwa = nazwa;
+        this.opis = opis;
+
+    }
+
+    public int getrasa_id() { return rasa_id; }
+    public void setrasa_id(int rasa_id) { this.rasa_id = rasa_id; }
+
+    public String getnazwa() { return  nazwa; }
+    public void setnazwa(String nazwa) { this.nazwa = nazwa; }
+
+    public String getopis() { return opis; }
+    public void setopis(String opis) { this.opis = opis; }
+}
